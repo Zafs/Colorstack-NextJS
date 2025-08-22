351e8b6e3964d65acf3a4c1e45b0ac039dd1895b
Zafs <Zaffs@protonmail.com>
2025-08-22 12:57:27 -0700

    Replace VercelInsights component with SpeedInsights from @vercel/speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from '@clerk/themes';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
-import VercelInsights from "@/components/VercelInsights";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -64,7 +64,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           {children}
-          <VercelInsights />
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
